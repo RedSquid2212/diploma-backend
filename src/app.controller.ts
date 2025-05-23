cf3530b0999d3157b1ff178db82c4a2b3c66007d
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-05-23 19:07:55 +0500

    Подключила MongoDB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { UsersService } from './modules/users/user.service';
+import { User } from './modules/users/user.schema';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test-db')
+  async getHello(): Promise<User> {
+    const user = await this.usersService.create();
+    console.log(user);
+    return user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://admin:test@127.0.0.1:27017/into-atoms?authSource=admin'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
